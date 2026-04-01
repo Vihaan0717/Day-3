00b4602da1633814a5926939f8dae4c94ca45423
Vihaan0717 <rohitkanna2004@gmail.com>
2026-04-01 21:50:38 +0530

    2nd prog

--- Untitled-1.py
+++ Untitled-1.py
@@ -2,4 +2,9 @@ name = input("Enter your name: " )
 print("hello " + name)
 
 price = 10
+price = 20 # <--python always compile line by line so its last value should be 20 for price -->
 print(price)
+
+full_name = "John Smith"
+age = 20 # <--integer-->
+is_new = True # <--boolean-->
