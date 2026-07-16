65dcf7c1836d40fc3638f42351cf6258247248b2
Vihaan0717 <rohitkanna2004@gmail.com>
2026-07-16 22:48:24 +0530

    added 16/07/26 name,color

--- start.py
+++ start.py
@@ -4,3 +4,7 @@ print(' ||||')
 
 fav_food='fried rice'
 print('my fav food was' + fav_food)
+
+name=input('what is your name?')
+fav=input('what is your favorite color?')
+print(name + ' likes ' + fav)
