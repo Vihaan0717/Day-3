ac7499ac993143bf587a45199142254bc31540c1
Vihaan0717 <rohitkanna2004@gmail.com>
2026-07-18 22:32:53 +0530

    added operators 18/07/26

--- start.py
+++ start.py
@@ -1,24 +1,8 @@
-print("hello Amma")
-print('0----')
-print(' ||||')
-
-fav_food='fried rice'
-print('my fav food was' + fav_food)
-
-name=input('what is your name?')
-fav=input('what is your favorite color?')
-print(name + ' likes ' + fav)
-
-for i in range(3):
-    print(i)
-
-born_year=input('what is your born year? ')
-age=2026 - int(born_year)
-print(age)
-
-"""For loop time compli"""
-for i in range(2):
-    for j in range(3):
-        print(i,j)
-
-        
+a=15
+b=25
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a%b)
+print(a**b)
--- test.py
+++ test.py
@@ -17,3 +17,32 @@ for i in range(3):
 for i in range(2):
     for j in range(3):
         print(i,j)
+
+"""DAY-1 (18/07/26)"""
+
+"""Print your name."""
+name=input('what is your name? ')
+print(name)
+"""Print your age."""
+age=input('what is your age? ')
+print(age)
+"""Take the user's name as input and greet them."""
+user_name=input('what is your name? ')
+print("hello," + user_name)
+"""Take two numbers as input and print their sum."""
+number_1=input('Enter the first number? ')
+number_2=input('Enter the second number? ')
+print(int(number_1)+int(number_2))
+"""Print the data type of:
+Integer
+Float
+String
+Boolean"""
+print(type(int(1919)))
+print(type(float(1.000)))
+print(type(str('kanna')))
+print(type(bool(True)))
+
+
+
+
