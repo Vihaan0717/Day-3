db0ed66354c1422aa4da53f6858ea042dca4c037
Vihaan0717 <rohitkanna2004@gmail.com>
2026-07-19 21:48:02 +0530

    added 19/07/26 find,replace,upper,lower,length

--- start.py
+++ start.py
@@ -1,8 +1,19 @@
-a=15
-b=25
-print(a+b)
-print(a-b)
-print(a*b)
-print(a/b)
-print(a%b)
-print(a**b)
+first='john'
+second='smith'
+message = first +' [' +second+ '] is the coder'
+print(message)
+"""let's find the word in a string"""
+course = 'python for beginners'
+print('python' in course)
+"""Lets find the length of the string"""
+course = 'python for beginners'
+print(len(course))
+"""Lets find the index of a string"""
+course = 'python for beginners'
+print(course.find('p'))
+"""Lets replace the string"""
+course = 'python for beginners'
+print(course.replace('beginners','absolute beginners'))
+"""Lets now make it upper case"""
+course = 'python for begginers'
+print(course.upper())
