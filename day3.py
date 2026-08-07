a2762b90bb1e4a2ecf10c17e1039ae55664ea273
Vihaan0717 <rohitkanna2004@gmail.com>
2026-08-07 21:22:21 +0530

    added India100

--- day3.py
+++ day3.py
@@ -1,1 +1,9 @@
+weight = input("weight: ")
+unit = input("(K)g or (L)g ?")
+if unit.upper == "L":
+    converted = weight * int(0.4)
+    print(f"your rate = {converted}")
+else:
+    converted = weight * int (2.3)
+    print(f"your rate = {converted}")
 
--- house_price = 1000000.py
+++ house_price = 1000000.py
@@ -1,11 +1,12 @@
-"""Re-call 02-08-26 Amma"""
-house_price = 1000000
-has_good_grade = True
-has_bad_grade = False
+"""RE-call 5-08-26//lets start with a question"""
+price_house = 10000000
+has_good_cridit = True
+has_bad_cridt = False
 
-if has_good_grade:
-    down_payment = 0.1 * house_price
-    print(f"Down payment: {down_payment}")
+if has_good_cridit:
+    down_pyment = 0.1 * price_house
+    print(f"Down Payment = {down_pyment}")
 else:
-    down_payment = 0.2 * house_price
-    print(f"Down payment: {down_payment}")
+    down_payment = 0.2 * price_house
+    print(f"Down Payment = {down_payment}")
+    
